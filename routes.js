74171740ee18d7e9ad7e1f74309378c36d3d768c
Kristina <ccthw@protonmail.com>
2020-07-04 12:36:18 +0300

    Fix bugs

--- routes.js
+++ routes.js
@@ -15,20 +15,25 @@ router.get('/timestamp', (req, res, next) => {
  * If the endpoint has a date in UNIX format, calculate the human readable format, and vice versa 
  */
 router.get('/timestamp/:date_string', (req, res, next) => {
-    const { date_string } = req.params; 
-    // if date_string is in human readable format 
-    if (/^([12]\d{3}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01]))$/.test(date_string)) {
-        const unix = parseInt((new Date(date_string).getTime() / 1000).toFixed(0));
-        const utc = new Date(unix * 1000).toUTCString();
-        res.json({ unix, utc });
-    } else if (/^-?\d*$/.test(date_string)) { // if date_string is in unix format
+
+    const { date_string } = req.params;
+
+    if (/\d{5,}/.test(date_string)) {
         const unix = parseInt(date_string);
-        const utc = new Date(unix * 1000).toUTCString();
-        res.json({ unix, utc }); 
-    } else {
+        const utc = new Date(unix).toUTCString();
+        res.json({ unix, utc });
+    } 
+
+    const date = new Date(date_string).toString();
+
+    if (date === 'Invalid Date') {
         const err = new Error('Invalid Date');
         err.status = 400;
         next(err); 
+    } else {
+        const unix = parseInt(new Date(date_string).getTime());
+        const utc = new Date(unix).toUTCString();
+        res.json({ unix, utc });
     }
 });
 
